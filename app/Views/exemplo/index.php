91dbf428607d1f41bf5310dba521d08085de1699
Magno <dimagno13@gmail.com>
2024-12-30 21:15:10 -0300

    criação esqueleto API service

--- app/Views/exemplo/index.php
+++ app/Views/exemplo/index.php
@@ -6,18 +6,38 @@
  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    
    
 </head>
 
  <body>
- <h1><?= $data['name']; ?></h1>
-<p>Versão: <?= $data['version']; ?></p>
-
-    
+   <header>
+      <a href="#" class ="logo">Logo</a>
+      <ul>
+         <li><a href="#" class="active">Home</a></li>
+         <li><a href="#">About</a></li>
+         <li><a href="#">Contact</a></li>
+         <li><a href="#">More</a></li>
+      </ul>
+   </header>
+   <section>
+      <img src="public/imgs/moon.png" alt="" class="moon">
+      <img src="public/imgs/stars.png" alt="" class="stars">
+      <img src="public/imgs/mountains_behind.png" class="mountain-b" alt="">
+      <h2 id="text">Star Wars</h2>
+      
+   <a href="#" class="btn" > Explore</a>
+      <img src="public/imgs/mountains_front.png" class="mountain-f" alt="">
+   </section>
+<div class="sec">
+   <h2>Parallax scrolling effects</h2>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit accusantium ad, aut nam corporis, maiores nostrum consequuntur consectetur voluptatem dolores officiis atque quam quidem ab ex, neque magni quia amet
+   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit accusantium ad, aut nam corporis, maiores nostrum consequuntur consectetur voluptatem dolores officiis atque quam quidem ab ex, neque magni quia amet.
+   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit accusantium ad, aut nam corporis, maiores nostrum consequuntur consectetur voluptatem dolores officiis atque quam quidem ab ex, neque magni quia amet
+   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit accusantium ad, aut nam corporis, maiores nostrum consequuntur consectetur voluptatem dolores officiis atque quam quidem ab ex, neque magni quia amet</p>
+</div>
  </body>
  </html>
-    </script><script src="/starwars/public/js/main.js"></script>
-
+ <link rel="stylesheet" href="/starwars/public/css/style.css">
+ </script><script src="/starwars/public/js/main.js"></script>
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
-    alert('ok')
-    fetch('https://swapi-node.vercel.app/films').then((data)=>{
+ fetch('https://swapi-node.vercel.app/films').then((data)=>{
         console.log('dados:'+data)
     }).catch((error)=>{
         console.log("erro :"+error)
     });
+    
+    let stars = $('.stars');
+    let moon = $('.moon');
+    let mountainB = $('.mountain-b');
+    let mountainF = $('.mountain-f');
+    let text = $('#text');
+    
+    $(window).on('scroll', function () {
+        let scrollValue = $(this).scrollTop(); // Obtém o valor do scroll vertical
+
+        // Aplica o efeito parallax em cada elemento
+        stars.css('left', scrollValue * 0.5 + 'px'); // As estrelas movem-se mais lentamente
+        moon.css('top', scrollValue * 1.15 + 'px');   // A lua move-se devagar para cima
+        mountainB.css('top', scrollValue * 0.2 + 'px'); // Montanha de trás, efeito suave
+        mountainF.css('top', scrollValue * 0 + 'px'); // Montanha da frente, movimento mais pronunciado
+      
+    });
 }); 
+
