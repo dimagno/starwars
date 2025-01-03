e79c8f9abce147deb79c291e741754a75d1794c0
Magno <dimagno13@gmail.com>
2025-01-02 22:06:25 -0300

    .

--- app/Views/films.php
+++ app/Views/films.php
@@ -1,5 +1,57 @@
 <?php include "layout/header.php" ?>
 <div class="films">
-    <h1 class="text-white text-center">Pagina de Filmes</h1>
+    <div class="header">
+        <div class="galeria row">
+            <div class="single-galeria col-sm-4 shadow shadow-lg border border-black">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <img src="public/imgs/films/ep1.jpg?w=500&q=60" class="img-fluid" alt="">
+
+                    <div class="info">
+                        <h1>Título</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam labore.</p>
+                        <a href="#">Ver Mais</a>
+                    </div>
+                </div>
+            </div>
+            <div class="single-galeria col-sm-4 shadow shadow-lg ">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <img src="public/imgs/films/ep1.jpg?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="">
+
+                    <div class="info">
+                        <h1>Título</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam labore.</p>
+                        <a href="#">Ver Mais</a>
+                    </div>
+                </div>
+            </div>
+            <div class="single-galeria col-sm-4 shadow-lg  ">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <img src="public/imgs/films/ep1.jpg?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="">
+
+                    <div class="info">
+                        <h1>Título</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam labore.</p>
+                        <a href="#">Ver Mais</a>
+                    </div>
+                </div>
+            </div>
+            
+
+
+        </div>
+    </div>
 </div>
 <?php include "layout/footer.php" ?>
+<style>
+    .single-galeria {
+        border-radius: 4px;
+      
+        
+    }
+
+    .single-galeria img {
+        max-width: 100%;
+        height: 300px;
+        margin: 2px 0px;
+    }
+</style>
